0d454b31ab2a8796408743230eb228bf6a47f21a
koko <koko@KOKO>
2024-10-18 21:24:05 +0800

    Add project files.

---
+++ Model/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace Model
+{
+    public class Student
+    {
+        public int StudentNumber { get; set; }
+        public string FullName { get; set; }
+        public decimal Grade { get; set; }
+    }
+}
